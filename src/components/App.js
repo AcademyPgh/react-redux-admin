c3607c51d0d820667bc1e33bf65c0b921d9dc8fb
Rory Dahl <rsdah13@gmail.com>
2017-07-25 12:52:50 +0200

    Routing fixed and added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 //This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header</p>
+        <Header />
         {this.props.children}
       </div>
     );
@@ -13,7 +14,7 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  children: Proptypes.object.isRequired
+  children: PropTypes.object.isRequired
 };
 
-export default App; 
+export default App;
