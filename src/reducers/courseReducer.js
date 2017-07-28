1661b20b81aadb9259c6e281f8b3a77440134a23
Rory Dahl <rsdah13@gmail.com>
2017-07-28 12:01:58 +0200

    9: Reducers ==> Create first reducer

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,11 @@
+//set default value for state in parameters
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case 'CREATE_COURSE':
+      return [...state,
+        Object.assign({}, action.course)
+      ];
+    default:
+      return state;
+  }
+}
