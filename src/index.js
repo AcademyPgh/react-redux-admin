2fd5871a1a82e018b7a7dc4642267f3864c2249a
Rory Dahl <rsdah13@gmail.com>
2017-07-30 12:08:59 +0200

    9: Instantiate Store and Provider ==> Wrap entrypoint with Provider that attaches store to react-container components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-// import configureStore from './store/configureStore';
+import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 // import {loadCourses} from './actions/courseActions';
 // import {loadAuthors} from './actions/authorActions';
@@ -11,7 +11,11 @@ import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
+const store = configureStore();
+
 render(
-   <Router history={browserHistory} routes={routes}/>,
-  document.getElementById('app')
+  <Provider store={store}> 
+     <Router history={browserHistory} routes={routes}/>,
+    document.getElementById('app')
+  </Provider>
 );
