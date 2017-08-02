86ed72c68262f11f2dad862f479a7966e093d074
Rory Dahl <rsdah13@gmail.com>
2017-08-02 18:09:27 +0200

    Create Save Course Thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,12 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course }; // omitted value 'course: course'. In ES6, you only need the key
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course }; // omitted value 'course: course'. In ES6, you only need the key
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course }; // omitted value 'course: course'. In ES6, you only need the key
 }
 
 export function loadCoursesSuccess(courses) {
@@ -17,6 +21,17 @@ export function loadCourses() {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
       throw error;
+    });
+  };
+}
+
+export function saveCourse(course){
+  return function(dispatch, getState) {
+    courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+      dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      throw(error);
     });
   };
 }
