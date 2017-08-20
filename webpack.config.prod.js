1ebabe8be0a5b2936b4d45fbc4c1c1d089b5ee9e
Rory Dahl <rsdah13@gmail.com>
2017-08-20 08:12:14 -0700

    Set Up Dist Server

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import path from 'path';
 
 const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production');
+  'process.env.NODE_ENV': JSON.stringify('production')
 };
 export default {
   debug: true,
